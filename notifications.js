87146bc3b5c3afe61e442a6c4931e34203c4657c
Nehemiah Langness <Nehemiah.Langness@hotmail.com>
2025-04-15 15:04:00 -0500

    Updates

--- notifications.js
+++ notifications.js
@@ -1,38 +1,38 @@
-self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener('install', () => self.skipWaiting());
 
-self.addEventListener("push", function (event) {
-  const data = event.data?.json() ?? {};
-  console.log("Received a push message", data);
+self.addEventListener('push', function (event) {
+    const data = event.data?.json() ?? {};
+    console.log('Received a push message', data);
 
-  if (!(self.Notification && self.Notification.permission === "granted")) {
-    return;
-  }
+    if (!(self.Notification && self.Notification.permission === 'granted')) {
+        return;
+    }
 
-  const title = data.title ?? "You have tasks due";
-  const body = data.body ?? "Open tasks.n-lang.dev to view them";
-  const icon = "/favicon.svg";
-  const tag = new Date().valueOf().toFixed();
+    const title = data.title ?? 'You have tasks due today';
+    const body = data.body ?? 'Open tasks.n-lang.dev to view them';
+    const tag = new Date().valueOf().toFixed();
 
-  event.waitUntil(
-    self.registration.showNotification(title, {
-      body: body,
-      icon: icon,
-      badge: icon,
-      image: "/tasks-due.jpeg",
-      tag: tag,
-      renotify: true,
-    })
-  );
+    event.waitUntil(
+        self.registration.showNotification(title, {
+            body: body,
+            icon: '/favicon.svg',
+            badge: '/logo-mask.svg',
+            image: '/notification-banner.png',
+            tag: tag,
+            renotify: true,
+            silent: false,
+        })
+    );
 });
 
-self.addEventListener("notificationclick", (event) => {
-  event.waitUntil(
-    self.clients.matchAll().then((clients) => {
-      if (clients.length) {
-        clients[0].focus();
-      } else {
-        self.clients.openWindow("/");
-      }
-    })
-  );
+self.addEventListener('notificationclick', (event) => {
+    event.waitUntil(
+        self.clients.matchAll().then((clients) => {
+            if (clients.length) {
+                clients[0].focus();
+            } else {
+                self.clients.openWindow('/');
+            }
+        })
+    );
 });
